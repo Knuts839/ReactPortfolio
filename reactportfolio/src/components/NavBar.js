f0b3071a6a2e68eba045c3b6cf21c777c3b6151e
Ravi <knuts839@umn.edu>
2020-11-23 18:48:39 -0600

    created navbar and wrapper and empty page files within page folder

---
+++ reactportfolio/src/components/NavBar.js
@@ -0,0 +1,34 @@
+  
+import React from "react";
+import "../../App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from "react-router-dom";
+
+
+const mainPage = () => {
+    return (
+        <div className="navApp">
+            <nav className="navbar">
+                <div className="col-md-1"></div>
+                <div className="col-md-3">
+                    <span id="name" className="navbar-brand mb-0 h1">Ravi Knutson</span>
+                </div>
+                <div className="col-md-3">
+
+                </div>
+                <div className="col-md-1">
+                    <Link to="/About">About</Link>
+                </div>
+                <div className="col-md-1">
+                <Link to="/Portfolio">Portfolio</Link>
+                </div>
+                <div className="col-md-1">
+                <Link to="/Contact">Contact</Link>
+                </div>
+                <div className="col-md-1"></div>
+            </nav>
+
+        </div>
+    )
+}
+export default mainPage
