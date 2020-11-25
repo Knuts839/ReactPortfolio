99ebc7e3d0f906135ea94ee9906847a09055a169
Ravi <knuts839@umn.edu>
2020-11-24 21:18:13 -0600

    dont want links collapsed on mobile

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,11 +8,6 @@ import { Link } from "react-router-dom";
 const mainPage = () => {
     return (
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
