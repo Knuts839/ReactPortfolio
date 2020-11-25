7c632e1550ea74918b536fca1e8d2bd96b8a3bca
Ravi <knuts839@umn.edu>
2020-11-24 21:22:41 -0600

    try this again

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,6 @@ import { Link } from "react-router-dom";
 const mainPage = () => {
     return (
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
                     <Link to= "/Home" class="nav-link" >Home</Link>
@@ -17,7 +16,6 @@ const mainPage = () => {
                     <Link to= "/ContactInfo" class="nav-link" >ContactInfo</Link>
                 </li>         
             </ul>
-        </div>
     </nav>
     )
 }
